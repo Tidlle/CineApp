e95c4dfb328a7c67ff3ad16a79179825d60cef04
Tidlle <98828582+Tidlle@users.noreply.github.com>
2024-06-12 09:38:57 -0300

    saiba mais

--- CineApp/src/main.jsx
+++ CineApp/src/main.jsx
@@ -8,6 +8,7 @@ import Filmes from './Pages/Filmes.jsx'
 import Sobre from './Pages/Sobre.jsx'
 import Contato from './Pages/Contato.jsx'
 import PageNotFound from './Pages/PageNotFound.jsx'
+import DetalhesFilme from './Pages/DetalhesFilme.jsx'
 
 const router = createBrowserRouter(
   [
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       children: [
         {index: true, element: <Home/>},
         {path: "filmes", element: <Filmes/>},
+        {path: "filmes/:id", element: <DetalhesFilme/>},
         {path: "sobre", element: <Sobre/>},
         {path: "contato", element: <Contato/>},
         {path: "*", element: <PageNotFound/>}
